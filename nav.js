adac2f5ec6baeeb99b88b9c0b8c72754dd330ca7
Xiaonan Wang <wxn.acad@gmail.com>
2026-04-18 20:55:05 +0900

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,14 @@
+// nav.js
+document.addEventListener("DOMContentLoaded", function() {
+    const navHTML = `
+        <a href="/index.html">Home</a>
+        <a href="/publications/index.html">Publications</a> 
+        <a href="/projects/index.html">Projects</a>
+        <a href="/services/index.html">Services</a>
+        <a href="/cv.html">CV</a>
+    `;
+    const navElement = document.querySelector('nav');
+    if (navElement) {
+        navElement.innerHTML = navHTML;
+    }
+});
